8abb999dc135aaab742d41ca2002d7c6ce33213e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2012-08-04 11:16:39 +0100

    Implement all the useful public functions
    There may be memory leaks in this code, I'm not too familiar with C

--- python-interface.c
+++ python-interface.c
@@ -4,15 +4,91 @@
 #include <Python.h>
 #include "megahal.h"
 
-static PyObject *mhinitbrain(PyObject *self, PyObject *args)
+/*
+ * Set methods
+ */
+
+static PyObject *mhsetnoprompt(PyObject *self, PyObject *args)
+{
+    megahal_setnoprompt();
+    return Py_BuildValue("");
+}
+
+static PyObject *mhsetnowrap(PyObject *self, PyObject *args)
+{
+    megahal_setnowrap();
+    return Py_BuildValue("");
+}
+
+static PyObject *mhsetnobanner(PyObject *self, PyObject *args)
+{
+    megahal_setnobanner();
+    return Py_BuildValue("");
+}
+
+static PyObject *mhseterrorfile(PyObject *self, PyObject *args)
 {
     char *input;
 
-    if (PyArg_ParseTuple(args, "s", &input))
-    megahal_setdirectory(input);
+    if (!PyArg_ParseTuple(args, "s", &input))
+    return NULL;
 
+    megahal_seterrorfile(input);
+    return Py_BuildValue("");
+}
+
+static PyObject *mhsetstatusfile(PyObject *self, PyObject *args)
+{
+    char *input;
+
+    if (!PyArg_ParseTuple(args, "s", &input))
+    return NULL;
+
+    megahal_setstatusfile(input);
+    return Py_BuildValue("");
+}
+
+static PyObject *mhsetdirectory(PyObject *self, PyObject *args)
+{
+    char *input;
+
+    if (!PyArg_ParseTuple(args, "s", &input))
+    return NULL;
+
+    megahal_setdirectory(input);
+    return Py_BuildValue("");
+}
+
+/*
+ * Main functions
+ */
+
+static PyObject *mhinitalize(PyObject *self, PyObject *args)
+{
     megahal_initialize();
     return Py_BuildValue("");
+}
+
+static PyObject *mhinitialgreeting(PyObject *self, PyObject *args)
+{
+    char *output = NULL;
+
+    output = megahal_initial_greeting();
+
+    return PyString_FromString(output);
+}
+
+static PyObject *mhcommand(PyObject *self, PyObject *args)
+{
+    char *input;
+    int *output = NULL;
+
+    if (!PyArg_ParrseTuple(args, "s", &input))
+    return NULL;
+
+    output = megahal_command(input)
+
+    return PyInt_FromLong(output)
 }
 
 static PyObject *mhdoreply(PyObject *self, PyObject *args)
@@ -25,7 +101,7 @@ static PyObject *mhdoreply(PyObject *self, PyObject *args)
 
     output = megahal_do_reply(input, 1);
 
-    return PyString_FromString(output);
+    return Py_BuildValue("s", output);
 }
 
 static PyObject *mhlearn(PyObject *self, PyObject *args)
@@ -47,7 +123,15 @@ static PyObject *mhcleanup(PyObject *self, PyObject *args)
 }
 
 static struct PyMethodDef mh_methods[] = {
-    {"initbrain", mhinitbrain, METH_VARARGS, "Initialize megahal brain"},
+    {"setnoprompt", mhsetnoprompt, METH_VARARGS, "Turn off prompt"},
+    {"setnowrap", mhsetnowrap, METH_VARARGS, "Turn off text wrap"}
+    {"setnobanner", mhsetnobanner, METH_VARARGS, "Turn off banner"}
+    {"seterrorfile", mhsetterrorfile, METH_VARARGS, "Set filename of error file"}
+    {"setstatusfile", mhsetstatusfile, METH_VARARGS, "Set filename of status file"}
+    {"setdirectory", mhsetdirectory, METH_VARARGS, "Set directory for files"}
+    {"initalize", mhinitalize, METH_VARARGS, "Initialize megahal brain"},
+    {"initialgreeting", mhinitialgreeting, METH_VARARGS, "Return initial greeting"}
+    {"command", mhcommand, METH_VARARGS, "Give Megahal a command"}
     {"doreply", mhdoreply, METH_VARARGS, "Generate a reply"},
     {"cleanup", mhcleanup, METH_VARARGS,"Clean megahal"},
     {"learn", mhlearn, METH_VARARGS, "Learn from a sentence, don't generate a reply"},
@@ -61,3 +145,4 @@ void initmegahal()
     if(PyErr_Occurred())
 	Py_FatalError("can't initialize module megahal");
 }
+
